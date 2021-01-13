72cb3e32ebbf62131d283e7c4c8eb391b692f6a5
hrishi3 <hrishikeshshinde6@gmail.com>
2021-01-13 09:45:40 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,47 +1,89 @@
-import flask
-import pickle
-import pandas as pd
+import os
+from flask import Flask, request, redirect, url_for, render_template, send_from_directory,flash 
+from werkzeug.utils import secure_filename
+import cv2
+import numpy as np
 
-# Use pickle to load in the pre-trained model
-with open(f'model/covid19detector (1).pkl', 'rb') as f:
-    model = pickle.load(f)
+UPLOAD_FOLDER ='static/uploads/'
+DOWNLOAD_FOLDER = 'static/downloads/'
+ALLOWED_EXTENSIONS = {'jpg', 'png','.jpeg'}
+app = Flask(__name__, static_url_path="/static")
 
-# Initialise the Flask app
-app = flask.Flask(__name__, template_folder='templates')
+
+# APP CONFIGURATIONS
+app.config['SECRET_KEY'] = 'opencv'  
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+app.config['DOWNLOAD_FOLDER'] = DOWNLOAD_FOLDER
+# limit upload size upto 6mb
+app.config['MAX_CONTENT_LENGTH'] = 6 * 1024 * 1024
+
+def allowed_file(filename):
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+
 @app.route('/', methods=['GET', 'POST'])
-def main():
-    if flask.request.method == 'GET':
-        return(flask.render_template('main3.html'))
-    if flask.request.method == 'POST':
-        
-        age = flask.request.form['age']
-        
-        gender = flask.request.form.get('gender')
-        
-        temprature = flask.request.form['temprature']
-        cough = flask.request.form.get('cough')
-        throat = flask.request.form.get('throat')
-        weak = flask.request.form.get('weak')
-        breath = flask.request.form.get('breath')
-        drow = flask.request.form.get('drow')
-        pain = flask.request.form.get('pain')
-        red = flask.request.form.get('red')
-        
-        input_variables = pd.DataFrame([[age,gender,temprature, cough,throat, weak,breath,drow,pain,red]])
-        
-        pred = model.predict(input_variables)[0]
-        
-        if int(pred)==0:
-          prediction='Low Risk'
-        elif int(pred)==1:
-          prediction='Moderate to High Risk'
-        else:
-          prediction='High Risk'
-          
-          
-          
-        return flask.render_template('main3.html',result=prediction)
+def index():
+    if request.method == 'POST':
+        if 'file' not in request.files:
+            flash('No file attached in request')
+            return redirect(request.url)
+        file = request.files['file']
+        if file.filename == '':
+            flash('No file selected')
+            return redirect(request.url)
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(UPLOAD_FOLDER, filename))
+            process_file(os.path.join(UPLOAD_FOLDER, filename), filename)
+            data={
+                "processed_img":'static/downloads/'+filename,
+                "uploaded_img":'static/uploads/'+filename
+            }
+            return render_template("index.html",data=data)  
+    return render_template('index.html')
+
+
+def process_file(path, filename):
+    detect_object(path, filename)
     
-    if __name__ == '__main__':
-        app.debug=True
-        app.run()
+
+def detect_object(path, filename):    
+    CLASSES = ["background", "aeroplane", "bicycle", "bird", "boat",
+        "bottle", "bus", "car", "cat", "chair", "cow", "diningtable",
+        "dog", "horse", "motorbike", "person", "pottedplant", "sheep",
+        "sofa", "train", "tvmonitor"]
+    COLORS = np.random.uniform(0, 255, size=(len(CLASSES), 3))
+    prototxt="ssd/MobileNetSSD_deploy.prototxt.txt"
+    model ="ssd/MobileNetSSD_deploy.caffemodel"
+    net = cv2.dnn.readNetFromCaffe(prototxt, model)
+    image = cv2.imread(path)
+    image = cv2.resize(image,(480,360))
+    (h, w) = image.shape[:2]
+    blob = cv2.dnn.blobFromImage(cv2.resize(image, (300, 300)), 0.007843, (300, 300), 127.5)
+    net.setInput(blob)
+    detections = net.forward()
+    for i in range(0, detections.shape[2]):
+        confidence = detections[0, 0, i, 2]
+        if confidence > 0.60:
+            idx = int(detections[0, 0, i, 1])
+            box = detections[0, 0, i, 3:7] * np.array([w, h, w, h])
+            (startX, startY, endX, endY) = box.astype("int")
+
+            # display the prediction
+            label = "{}: {:.2f}%".format(CLASSES[idx], confidence * 100)
+            # print("[INFO] {}".format(label))
+            cv2.rectangle(image, (startX, startY), (endX, endY),
+                COLORS[idx], 2)
+            y = startY - 15 if startY - 15 > 15 else startY + 15
+            cv2.putText(image, label, (startX, y),
+                cv2.FONT_HERSHEY_SIMPLEX, 0.5, COLORS[idx], 2)
+
+    cv2.imwrite(f"{DOWNLOAD_FOLDER}{filename}",image)
+  
+# download 
+# @app.route('/uploads/<filename>')
+# def uploaded_file(filename):
+#     return send_from_directory(app.config['DOWNLOAD_FOLDER'], filename, as_attachment=True)
+
+if __name__ == '__main__':
+    app.run()
